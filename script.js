37b4a374e5ac45145f5c1f1f3db4953134b458b0
unbearabletruth <antaarty@gmail.com>
2023-03-24 17:42:25 +0700

    Added simple UI to the project
    3 buttons for each game choice,
    used DOM methods to display results on the page

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+computer_win_count = 0;
+human_win_count = 0;
+
+
+
 function getComputerChoice(){
     var things = ['Rock', 'Paper', 'Scissors'];
     return things[Math.floor(Math.random()*things.length)];
@@ -18,31 +23,37 @@ function play_round(playerSelection, computerSelection){
     }  
 }
 
-function game(){
-    console.log("Welcome to the \"Rock, Paper, Scissors\" game!")
-    computer_win_count = 0;
-    human_win_count = 0;
-    let i = 1;
-    while (i <= 5){
-        your_choice = prompt("choose what you're playing with:");
-        let winner = play_round(your_choice.toLowerCase(), getComputerChoice().toLowerCase());
-        if (winner === 1){
-            human_win_count++;
-            console.log(`You win! Computer:${computer_win_count} vs Human:${human_win_count}`)
-        } else if (winner === 0){
-            computer_win_count++;
-            console.log(`Computer wins! Computer:${computer_win_count} vs Human:${human_win_count}`)
-        } else {
-            console.log("Draw! Nobody wins!")
-        }
-        i++;
+function game(which){
+    const your_choice = which;
+    let winner = play_round(your_choice.toLowerCase(), getComputerChoice().toLowerCase());
+    if (winner === 1){
+        human_win_count++;
+        const uscore = document.getElementById("uscore");
+        uscore.textContent = human_win_count;
+        console.log(`You win! Computer:${computer_win_count} vs Human:${human_win_count}`)
+    } else if (winner === 0){
+        computer_win_count++;
+        const cscore = document.getElementById("cscore");
+        cscore.textContent = computer_win_count;
+        console.log(`Computer wins! Computer:${computer_win_count} vs Human:${human_win_count}`)
+    } else {
+        console.log("Draw! Nobody wins!")
     }
-    if (computer_win_count > human_win_count){
-        return `Computer wins! Computer:${computer_win_count} vs Human:${human_win_count}`
-    } else if (computer_win_count < human_win_count){
-        return `You win! Computer:${computer_win_count} vs Human:${human_win_count}`
-    } else (computer_win_count === human_win_count)
-        return `Draw! Computer:${computer_win_count} vs Human:${human_win_count}`
+    if (computer_win_count === 5){
+        const winner = document.getElementById("winner");
+        winner.textContent = `Computer wins! Computer:${computer_win_count} vs Human:${human_win_count}`
+    } else if (human_win_count === 5){
+        const winner = document.getElementById("winner");
+        winner.textContent = `You win! Computer:${computer_win_count} vs Human:${human_win_count}`
+    }
 }
 
-console.log(game())
+  const rock_button = document.getElementById("Rck");
+  rock_button.addEventListener('click', () => game(rock_button.textContent));
+  
+  const paper_button = document.getElementById("Ppr");
+  paper_button.addEventListener('click', () => game(paper_button.textContent));
+
+  const scissors_button = document.getElementById("Scr");
+  scissors_button.addEventListener('click', () => game(scissors_button.textContent));
+ 
